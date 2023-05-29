979c8faa4242207805783f06cae298c54ca5f86d
alannnnes01 <alanjavierestradamonroy@gmail.com>
2023-05-29 00:38:56 -0600

    Cambios menores XD

--- js/traductor.js
+++ js/traductor.js
@@ -49,7 +49,6 @@ function traducirCoddevAJ() {
 
     var texto_traducido = traducirTextoEspecializado(texto_es);
     document.getElementById('resultado_coddev').textContent = 'La traducción del texto de CODDEV-AJ es: ' + texto_traducido;
-    document.getElementById('codigo_coddev').textContent = 'Código de traducción: ' + texto_traducido;
 }
 
 function traducirEspañolLatam() {
@@ -103,7 +102,7 @@ function traducirEspañolLatam() {
 
     var texto_traducido = traducirTextoNormal(texto_coddev);
     document.getElementById('resultado_es').textContent = 'La traducción del texto de ESPAÑOL LATAM es: ' + texto_traducido;
-    document.getElementById('codigo_es').textContent = 'Código de traducción: ' + texto_traducido;
+
     mostrarCodigoTraduccion(texto_traducido, 'espanol');
 }
 
@@ -121,3 +120,35 @@ inputTraducirCoddev.addEventListener('input', traducirEspañolLatam);
 
 var inputTraducirEs = document.getElementById('traducir_es');
 inputTraducirEs.addEventListener('input', traducirCoddevAJ);
+
+function copiarResultado(elementId) {
+  const resultadoElement = document.getElementById(elementId);
+  const textoCopiado = resultadoElement.textContent;
+  const textoSinTraduccion = textoCopiado.replace(/^La traducción del texto de .+ es: /, '');
+
+  // Crear un elemento de textarea temporal para copiar el texto sin la traducción
+  const textareaTemporal = document.createElement('textarea');
+  textareaTemporal.value = textoSinTraduccion;
+  document.body.appendChild(textareaTemporal);
+
+  // Seleccionar y copiar el contenido del textarea
+  textareaTemporal.select();
+  document.execCommand('copy');
+
+  // Eliminar el textarea temporal
+  document.body.removeChild(textareaTemporal);
+
+  // Mostrar un mensaje de éxito
+  const mensaje = document.createElement('div');
+  mensaje.classList.add('alert', 'alert-success');
+  mensaje.textContent = '¡Texto copiado!';
+  document.body.appendChild(mensaje);
+
+  // Desvanecer y eliminar el mensaje después de 2 segundos
+  setTimeout(function() {
+    mensaje.classList.add('fade');
+    setTimeout(function() {
+      document.body.removeChild(mensaje);
+    }, 1000);
+  }, 2000);
+}
